8a68d4b1ad92e7d1726a0baba10d5fa781f733ab
JNHFLINT <123217021+JNHFLINT@users.noreply.github.com>
2025-07-28 03:09:45 +0000

    Updated functionality to keep looping until quit

--- area_calculator.py
+++ area_calculator.py
@@ -10,25 +10,27 @@ print('3) Square')
 print('4) Circle')
 print('5) Quit')
 
-chosen_shape = int(input("Which shape: "))
+while True:
 
-if chosen_shape == 1:
-    height = int(input("Height: "))
-    base = int(input("Base: "))
-    area = (height * base) / 2
-    print('The area is:', int(area))
-if chosen_shape == 2:
-    length = int(input("Length: "))
-    width = int(input("Width: "))
-    area = length * width
-    print('The area is:', int(area))
-if chosen_shape == 3:
-    side = int(input("Side: "))
-    area = side * side
-    print('The area is:', int(area))
-if chosen_shape == 4:
-    radius = int(input("Radius: "))
-    area = math.pi * (radius * radius)
-    print('The area is:', int(area))
-if chosen_shape == 5:
-    quit
+    chosen_shape = int(input("Which shape: "))
+
+    if chosen_shape == 1:
+        height = int(input("Height: "))
+        base = int(input("Base: "))
+        area = (height * base) / 2
+        print('The area is:', int(area))
+    elif chosen_shape == 2:
+        length = int(input("Length: "))
+        width = int(input("Width: "))
+        area = length * width
+        print('The area is:', int(area))
+    elif chosen_shape == 3:
+        side = int(input("Side: "))
+        area = side * side
+        print('The area is:', int(area))
+    elif chosen_shape == 4:
+        radius = int(input("Radius: "))
+        area = math.pi * (radius * radius)
+        print('The area is:', int(area))
+    elif chosen_shape == 5:
+        break
